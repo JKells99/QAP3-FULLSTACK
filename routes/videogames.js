49f1cd518d6e7010dc86d58e109909b90c3e229c
JKells99 <j_kelloway@hotmail.com>
2023-03-29 08:49:41 -0230

    Added the abiltiy to search by Id and restructured

---
+++ routes/videogames.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router()
+
+const gamesDal = require('../services/pg.games.dal')
+
+router.get('/', async (req, res) =>{
+    try {
+        let theGames = await gamesDal.getGames();
+        if(DEBUG) console.table(theGames)
+        res.render('games', {theGames})
+    } catch{
+        res.render('503')
+        
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        let aGame = await gamesDal.getGameByGameId(req.params.id);
+        if (aGame.length === 0)
+            res.render('norecord')
+        else
+            res.render('game', {aGame});
+    } catch {
+        res.render('503');
+    }
+});
+
+
+router.post('/', async (req, res) => {
+    if(DEBUG) console.log("games.POST");
+    try {
+        await gamesDal.addGame( req.body.gameName, req.body.gameGenre );
+        res.redirect('/videogames/');
+    } catch {
+        // log this error to an error log file.
+        res.render('503');
+    } 
+});
+
+
+
+module.exports = router
---
+++ services/pg.games.dal.js
@@ -0,0 +1,55 @@
+const dal = require("./auth_db");
+
+var getGames = function() {
+    if(DEBUG) console.log("games.pg.dal.getGames()");
+    return new Promise(function(resolve, reject) {
+      const sql = "SELECT game_id AS _id, game_name,game_genre FROM videogame1"
+      dal.query(sql, [], (err, result) => {
+        if (err) {
+          // logging should go here
+          if(DEBUG) console.log(err);
+          reject(err);
+        } else {
+          resolve(result.rows);
+        }
+      }); 
+    }); 
+  };
+
+  var getGameByGameId = function(id) {
+    if(DEBUG) console.log("games.pg.dal.getGameByGameId()");
+    return new Promise(function(resolve, reject) {
+      const sql = "SELECT game_id AS _id, game_name, game_genre FROM videogame1 WHERE game_id = $1";
+      dal.query(sql, [id], (err, result) => {
+        if (err) {
+          // logging should go here
+          if(DEBUG) console.log(err);
+          reject(err);
+        } else {
+          resolve(result.rows);
+        }
+      }); 
+    }); 
+  };
+
+
+  var addGame = function(gamename, genre) {
+    if(DEBUG) console.log("games.pg.dal.addGame()");
+    return new Promise(function(resolve, reject) {
+      const sql = "INSERT INTO videogame1 ( game_name, game_genre)VALUES ($1, $2);";
+      dal.query(sql, [gamename, genre], (err, result) => {
+        if (err) {
+            if(DEBUG) console.log(err);
+            reject(err);
+          } else {
+            resolve(result.rows);
+          }
+      }); 
+    });
+  };
+
+  module.exports = {
+    getGames,
+    addGame,
+    getGameByGameId
+  }
